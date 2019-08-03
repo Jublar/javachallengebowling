73f2bdc64d84580a803822c0860eeb5e98d1ccd6
jublar.garcia <jublar@gmail.com>
2019-08-03 10:00:02 -0500

    Adding attributes

--- src/main/java/ec/com/java/challenge/bowling/game/TenPinBowlingGame.java
+++ src/main/java/ec/com/java/challenge/bowling/game/TenPinBowlingGame.java
@@ -1,6 +1,11 @@
 package ec.com.java.challenge.bowling.game;
 
+import java.util.List;
+
 public class TenPinBowlingGame implements IBowlingGame{
+
+    private List<BowlingFrame> frames;
+    private String playerName;
 
     public void roll(int pins) {
 
