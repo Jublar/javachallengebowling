f578f4a7b4dd8c9abe6c8840ddf01ead1eb6d0e6
jublar.garcia <jublar@gmail.com>
2019-08-03 10:19:34 -0500

    Implementing tab separated line validation

--- src/main/java/ec/com/java/challenge/bowling/input/validator/FileTabGameInputValidator.java
+++ src/main/java/ec/com/java/challenge/bowling/input/validator/FileTabGameInputValidator.java
@@ -3,6 +3,24 @@ package ec.com.java.challenge.bowling.input.validator;
 public class FileTabGameInputValidator implements IGameInputValidator {
     @Override
     public boolean validate(Object obj) {
+        if(obj!=null) {
+            String line = obj.toString();
+            String[] lineSplit = line.trim().split("\\s+");
+            if(lineSplit.length == 2) {
+                if(lineSplit[1].toUpperCase().equals("F"))
+                    return true;
+                else {
+                    int pins = 0;
+                    try {
+                        pins = Integer.parseInt(lineSplit[1]);
+                        if (pins >=0 && pins <=10)
+                            return true;
+                    } catch (NumberFormatException ex) {
+                        return false;
+                    }
+                }
+            }
+        }
         return false;
     }
 }
