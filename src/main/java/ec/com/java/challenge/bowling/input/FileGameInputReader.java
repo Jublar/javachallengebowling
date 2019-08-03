159c3826dc9399435e6388bdb0a80d3da2a11b91
jublar.garcia <jublar@gmail.com>
2019-08-03 10:16:20 -0500

    Reading line by line file, and validating it

--- src/main/java/ec/com/java/challenge/bowling/input/FileGameInputReader.java
+++ src/main/java/ec/com/java/challenge/bowling/input/FileGameInputReader.java
@@ -2,7 +2,14 @@ package ec.com.java.challenge.bowling.input;
 
 import ec.com.java.challenge.bowling.input.validator.IGameInputValidator;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FileGameInputReader implements IGameInputReader {
 
@@ -16,6 +23,18 @@ public class FileGameInputReader implements IGameInputReader {
 
     @Override
     public List<String> read() {
-        return null;
+        List<String> fileLines = null;
+        if(fileName != null) {
+            File file = new File(fileName);
+            if(file.exists()) {
+                try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+                    fileLines = stream.collect(Collectors.toList());
+                    fileLines.stream().filter(l -> inputValidator.validate(l));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return fileLines;
     }
 }
