543e47aef9421ddfdc81685c213a063e682b7d39
jublar.garcia <jublar@gmail.com>
2019-08-03 09:51:10 -0500

    Adding functionality methods

--- src/main/java/ec/com/java/challenge/bowling/game/TenPinBowlingGame.java
+++ src/main/java/ec/com/java/challenge/bowling/game/TenPinBowlingGame.java
@@ -1,4 +1,16 @@
 package ec.com.java.challenge.bowling.game;
 
-public class TenPinBowlingGame {
+public class TenPinBowlingGame implements IBowlingGame{
+
+    public void roll(int pins) {
+
+    }
+
+    public int score() {
+        return 0;
+    }
+
+    public String playerName() {
+        return null;
+    }
 }
