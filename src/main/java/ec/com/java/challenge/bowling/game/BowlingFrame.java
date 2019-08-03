1ef3bfa83d0c19156b7cb8c2eb42241a17947d12
jublar.garcia <jublar@gmail.com>
2019-08-03 09:58:30 -0500

    Adding turns

--- src/main/java/ec/com/java/challenge/bowling/game/BowlingFrame.java
+++ src/main/java/ec/com/java/challenge/bowling/game/BowlingFrame.java
@@ -1,5 +1,10 @@
 package ec.com.java.challenge.bowling.game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BowlingFrame {
 
+    List<BowlingTurn> turns = new ArrayList<>();
+    
 }
