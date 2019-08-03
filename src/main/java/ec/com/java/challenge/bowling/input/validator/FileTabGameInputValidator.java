fdb12ca0bf78ad2acf524ab33316d34f415afc87
jublar.garcia <jublar@gmail.com>
2019-08-03 10:13:05 -0500

    Adding file validator

---
+++ src/main/java/ec/com/java/challenge/bowling/input/validator/FileTabGameInputValidator.java
@@ -0,0 +1,8 @@
+package ec.com.java.challenge.bowling.input.validator;
+
+public class FileTabGameInputValidator implements IGameInputValidator {
+    @Override
+    public boolean validate(Object obj) {
+        return false;
+    }
+}
---
+++ src/main/java/ec/com/java/challenge/bowling/input/validator/IGameInputValidator.java
@@ -0,0 +1,5 @@
+package ec.com.java.challenge.bowling.input.validator;
+
+public interface IGameInputValidator {
+    boolean validate(Object obj);
+}
