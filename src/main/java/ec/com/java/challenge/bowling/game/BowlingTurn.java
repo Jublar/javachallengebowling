721d54cdc37e252ebe9a3b2d0ecbf5ae6aecc710
jublar.garcia <jublar@gmail.com>
2019-08-03 09:53:09 -0500

    Adding class BowlingTurn

---
+++ src/main/java/ec/com/java/challenge/bowling/game/BowlingTurn.java
@@ -0,0 +1,14 @@
+package ec.com.java.challenge.bowling.game;
+
+public class BowlingTurn {
+
+    private int pins;
+
+    public void setPins(int pins) {
+        this.pins = pins;
+    }
+
+    public int getPins() {
+        return pins;
+    }
+}
