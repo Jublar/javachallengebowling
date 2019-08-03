a62567d9d93e6a407344d0ecd298460ea3a54cd9
jublar.garcia <jublar@gmail.com>
2019-08-03 10:13:48 -0500

    Adding attributes

--- src/main/java/ec/com/java/challenge/bowling/input/FileGameInputReader.java
+++ src/main/java/ec/com/java/challenge/bowling/input/FileGameInputReader.java
@@ -1,8 +1,18 @@
 package ec.com.java.challenge.bowling.input;
 
+import ec.com.java.challenge.bowling.input.validator.IGameInputValidator;
+
 import java.util.List;
 
 public class FileGameInputReader implements IGameInputReader {
+
+    private String fileName;
+    private IGameInputValidator inputValidator;
+
+    public FileGameInputReader(String fileName, IGameInputValidator inputValidator){
+        this.fileName = fileName;
+        this.inputValidator = inputValidator;
+    }
 
     @Override
     public List<String> read() {
