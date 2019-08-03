f4473ba7c2226b09bfed7a5815d9a2a1fed64b93
jublar.garcia <jublar@gmail.com>
2019-08-03 11:55:39 -0500

    Implementing method to calculate score of single frame

--- src/main/java/ec/com/java/challenge/bowling/game/TenPinBowlingGame.java
+++ src/main/java/ec/com/java/challenge/bowling/game/TenPinBowlingGame.java
@@ -14,11 +14,12 @@ public class TenPinBowlingGame implements IBowlingGame{
 
     public TenPinBowlingGame(String playerName) {
         frames = new ArrayList<>();
+        frames.add(new BowlingFrame());
         this.playerName = playerName;
     }
 
     public void roll(int pins) {
-        BowlingFrame currentFrame = frames.get(frames.size() - 1);
+        BowlingFrame currentFrame = (frames.size() == 0) ? new BowlingFrame() : frames.get(frames.size() - 1);
         boolean isBonusFrame = Arrays.asList(BONUS_FRAME_INDEXES).contains(frames.size() - 1);
         int turnsAllowed = isBonusFrame ? TURN_PER_BONUS_FRAME : TURN_PER_FRAME;
         boolean turnAvailable = currentFrame.turns.size() < turnsAllowed;
@@ -39,5 +40,46 @@ public class TenPinBowlingGame implements IBowlingGame{
 
     public String playerName() {
         return playerName;
+    }
+
+    @Override
+    public int frameScore(int frameNumber) {
+        int score = 0;
+        int nextSteps = 0;
+        if(frameNumber > 0 && frameNumber <= frames.size()) {
+            BowlingFrame frame = frames.get(frameNumber - 1);
+            if(frame.isStrike()) {
+                score += 10;
+                nextSteps = 2;
+            } else if(frame.isSpare()) {
+                score += 10;
+                nextSteps = 1;
+            } else {
+                score = frame.turns.stream().mapToInt(t -> t.getPins()).sum();
+            }
+            if(nextSteps > 0) {
+                score += frameRecursion(frameNumber, nextSteps);
+            }
+        }
+        return score;
+    }
+
+    private int frameRecursion(int index, int nextSteps) {
+        int score = 0;
+        if(index >= 0 && index < frames.size()) {
+            BowlingFrame frame = frames.get(index);
+            if(frame.isStrike()) {
+                score += 10;
+                nextSteps = 1;
+            } else if(frame.isSpare()) {
+                score += 10;
+            } else {
+                score = frame.turns.stream().mapToInt(t -> t.getPins()).sum();
+            }
+            if(nextSteps > 0) {
+                score += frameRecursion(index, nextSteps);
+            }
+        }
+        return score;
     }
 }
