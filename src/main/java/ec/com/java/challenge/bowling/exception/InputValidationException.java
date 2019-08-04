cdc2d8842d0911ae4002f1fd3e4db90575daad00
jublar.garcia <jublar@gmail.com>
2019-08-04 15:08:32 -0500

    New exception type to represent input error

---
+++ src/main/java/ec/com/java/challenge/bowling/exception/InputValidationException.java
@@ -0,0 +1,9 @@
+package ec.com.java.challenge.bowling.exception;
+
+public class InputValidationException extends RuntimeException {
+
+    public InputValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
