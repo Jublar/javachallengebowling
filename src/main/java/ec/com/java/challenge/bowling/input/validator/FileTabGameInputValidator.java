98e92a72d25fd8e35538a9b83ed61aa6ffa7f726
jublar.garcia <jublar@gmail.com>
2019-08-03 19:57:24 -0500

    Using util class to split tab-separated line and validate it

--- src/main/java/ec/com/java/challenge/bowling/input/validator/FileTabGameInputValidator.java
+++ src/main/java/ec/com/java/challenge/bowling/input/validator/FileTabGameInputValidator.java
@@ -1,25 +1,14 @@
 package ec.com.java.challenge.bowling.input.validator;
 
+import ec.com.java.challenge.bowling.util.GameLineUtil;
+
 public class FileTabGameInputValidator implements IGameInputValidator {
     @Override
     public boolean validate(Object obj) {
         if(obj!=null) {
             String line = obj.toString();
-            String[] lineSplit = line.trim().split("\\s+");
-            if(lineSplit.length == 2) {
-                if(lineSplit[1].toUpperCase().equals("F"))
-                    return true;
-                else {
-                    int pins;
-                    try {
-                        pins = Integer.parseInt(lineSplit[1]);
-                        if (pins >=0 && pins <=10)
-                            return true;
-                    } catch (NumberFormatException ex) {
-                        return false;
-                    }
-                }
-            }
+            int pins = GameLineUtil.pins(line);
+            return pins >= 0 && pins <= 10;
         }
         return false;
     }
