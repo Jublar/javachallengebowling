3e760257e75aeb7dc66875226b9c534cecd749a7
jublar.garcia <jublar@gmail.com>
2019-08-04 16:10:10 -0500

    Method refactor, using map to store/retrieve games during parsing process

--- src/main/java/ec/com/java/challenge/bowling/parser/TenPinBowlingGameLineParser.java
+++ src/main/java/ec/com/java/challenge/bowling/parser/TenPinBowlingGameLineParser.java
@@ -5,8 +5,10 @@ import ec.com.java.challenge.bowling.game.TenPinBowlingGame;
 import ec.com.java.challenge.bowling.util.GameLineUtil;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * <p>TenPinBowlingGameLineParser class.</p>
@@ -14,7 +16,7 @@ import java.util.Objects;
  * @author Jublar Garcia Ramos
  * @version 1.0
  */
-public class TenPinBowlingGameLineParser implements IBowlingGameParser{
+public class TenPinBowlingGameLineParser implements IBowlingGameParser {
     private List<String> lines;
 
     /**
@@ -26,28 +28,36 @@ public class TenPinBowlingGameLineParser implements IBowlingGameParser{
         this.lines = lines;
     }
 
-    /** {@inheritDoc} */
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public IBowlingGame createSingle() {
         return null;
     }
 
-    /** {@inheritDoc} */
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public List<IBowlingGame> createAll() {
-        List<IBowlingGame> games = new ArrayList<>();
-        if(lines != null) {
-            lines.stream().forEach(l-> {
-                String playerName = GameLineUtil.playerName(l);
-                int pins = GameLineUtil.pins(l);
-                IBowlingGame game = games.stream().filter(g-> Objects.equals(playerName, g.playerName())).findAny().orElse(createNewGame(playerName));
-                game.roll(pins, GameLineUtil.isFoul(l));
-                if(!games.contains(game))
-                    games.add(game);
-            });
-
+        if (lines == null) {
+            return Collections.emptyList();
         }
-        return games;
+        Map<String, IBowlingGame> games = new HashMap<>();
+        lines.stream().forEach(line -> {
+            String playerName = GameLineUtil.playerName(line);
+            IBowlingGame game;
+            if (games.containsKey(playerName)) {
+                 game = games.get(playerName);
+            } else {
+                game = createNewGame(playerName);
+                games.put(playerName, game);
+            }
+            int pins = GameLineUtil.pins(line);
+            game.roll(pins, GameLineUtil.isFoul(line));
+        });
+        return new ArrayList<>(games.values());
     }
 
     private IBowlingGame createNewGame(String playerName) {
