33de92124facfc61ecb9bcf3a5eb1d881717eadd
jublar.garcia <jublar@gmail.com>
2019-08-03 20:06:01 -0500

    Implementing method to create games from all lines read from file

--- src/main/java/ec/com/java/challenge/bowling/parser/TenPinBowlingGameLineParser.java
+++ src/main/java/ec/com/java/challenge/bowling/parser/TenPinBowlingGameLineParser.java
@@ -1,8 +1,12 @@
 package ec.com.java.challenge.bowling.parser;
 
 import ec.com.java.challenge.bowling.game.IBowlingGame;
+import ec.com.java.challenge.bowling.game.TenPinBowlingGame;
+import ec.com.java.challenge.bowling.util.GameLineUtil;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class TenPinBowlingGameLineParser implements IBowlingGameParser{
     private List<String> lines;
@@ -18,6 +22,22 @@ public class TenPinBowlingGameLineParser implements IBowlingGameParser{
 
     @Override
     public List<IBowlingGame> createAll() {
-        return null;
+        List<IBowlingGame> games = new ArrayList<>();
+        if(lines != null) {
+            lines.stream().forEach(l-> {
+                String playerName = GameLineUtil.playerName(l);
+                int pins = GameLineUtil.pins(l);
+                IBowlingGame game = games.stream().filter(g-> Objects.equals(playerName, g.playerName())).findAny().orElse(createNewGame(playerName));
+                game.roll(pins);
+                if(!games.contains(game))
+                    games.add(game);
+            });
+
+        }
+        return games;
+    }
+
+    private IBowlingGame createNewGame(String playerName) {
+        return new TenPinBowlingGame(playerName);
     }
 }
