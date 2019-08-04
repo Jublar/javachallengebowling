5ee067a2d6291b3421dc1c1cf506d7483d92f167
jublar.garcia <jublar@gmail.com>
2019-08-04 15:23:31 -0500

    Using error handling in file reader constructor

--- src/main/java/ec/com/java/challenge/bowling/input/FileGameInputReader.java
+++ src/main/java/ec/com/java/challenge/bowling/input/FileGameInputReader.java
@@ -7,7 +7,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -18,7 +17,7 @@ public class FileGameInputReader implements IGameInputReader {
     private IGameInputValidator inputValidator;
 
     public FileGameInputReader(String fileName, IGameInputValidator inputValidator) throws IOException {
-        if (fileName == null || fileName == "") {
+        if (fileName == null || "".equals(fileName)) {
             throw new IllegalArgumentException("Argument fileName is required.");
         }
         if(!Files.exists(Paths.get(fileName))) {
