58ad24c3526c33b6492b4417b80df272f4a77d6b
jublar.garcia <jublar@gmail.com>
2019-08-04 11:20:13 -0500

    Adding header line to console out

--- src/main/java/ec/com/java/challenge/bowling/output/ConsoleGameOutputWriter.java
+++ src/main/java/ec/com/java/challenge/bowling/output/ConsoleGameOutputWriter.java
@@ -1,13 +1,25 @@
 package ec.com.java.challenge.bowling.output;
 
 import ec.com.java.challenge.bowling.game.IBowlingGame;
+import ec.com.java.challenge.bowling.util.Constants;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 public class ConsoleGameOutputWriter implements IGameOutputWriter {
 
     @Override
     public void write(List<IBowlingGame> games) {
-        
+        System.out.println(headerLine());
+    }
+
+    private static String headerLine() {
+        StringBuilder line = new StringBuilder();
+        line.append(Constants.MSG_FRAME);
+        IntStream stream1 = IntStream.range(1, Constants.MAX_FRAMES + 1);
+        stream1.forEach(s -> line.append("\t").append("\t").append(s));
+        return line.toString();
     }
 }
