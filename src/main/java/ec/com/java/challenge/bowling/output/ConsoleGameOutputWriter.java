6a7b6a286e4003722231a84c27cc9653dc1b3326
jublar.garcia <jublar@gmail.com>
2019-08-04 12:12:51 -0500

    Adding method to write game line, pinfalls and score

--- src/main/java/ec/com/java/challenge/bowling/output/ConsoleGameOutputWriter.java
+++ src/main/java/ec/com/java/challenge/bowling/output/ConsoleGameOutputWriter.java
@@ -1,5 +1,7 @@
 package ec.com.java.challenge.bowling.output;
 
+import ec.com.java.challenge.bowling.game.BowlingFrame;
+import ec.com.java.challenge.bowling.game.BowlingTurn;
 import ec.com.java.challenge.bowling.game.IBowlingGame;
 import ec.com.java.challenge.bowling.util.Constants;
 
@@ -10,16 +12,65 @@ import java.util.stream.IntStream;
 
 public class ConsoleGameOutputWriter implements IGameOutputWriter {
 
+    private static final String SEPARATOR = "\t";
+
     @Override
     public void write(List<IBowlingGame> games) {
         System.out.println(headerLine());
+        games.stream().forEach(g->gameLine(g).stream().forEach(l -> System.out.println(l)));
     }
 
     private static String headerLine() {
         StringBuilder line = new StringBuilder();
         line.append(Constants.MSG_FRAME);
-        IntStream stream1 = IntStream.range(1, Constants.MAX_FRAMES + 1);
-        stream1.forEach(s -> line.append("\t").append("\t").append(s));
+        IntStream stream1 = IntStream.range(0, Constants.MAX_FRAMES);
+        stream1.forEach(s -> line.append("\t").append("\t").append(s + 1));
         return line.toString();
+    }
+
+    private static List<String> gameLine(IBowlingGame game) {
+        List<String> lines = new ArrayList<>();
+        lines.add(game.playerName());
+        StringBuilder line = new StringBuilder();
+        line.append(Constants.MSG_PINFALLS);
+        IntStream pinFalls = IntStream.range(0, Constants.MAX_FRAMES);
+        IntStream score = IntStream.range(0, Constants.MAX_FRAMES);
+        pinFalls.forEach(p -> line.append(framePinFall(game, p)));
+        lines.add(line.toString());
+        StringBuilder scoreLine = new StringBuilder();
+        scoreLine.append(Constants.MSG_SCORE);
+        score.forEach(s -> scoreLine.append("\t").append("\t").append(String.valueOf(game.frameScore(s))));
+        lines.add(scoreLine.toString());
+
+        return lines;
+    }
+
+    private static String framePinFall(IBowlingGame game, int frameIndex) {
+        BowlingFrame frame = game.getFrame(frameIndex);
+        if(frame != null) {
+            if(frame.isStrike())
+                return String.format("%s%s%s", SEPARATOR, SEPARATOR, Constants.MSG_STRIKE_PINFALL);
+            else if(frame.isSpare()) {
+                String turn1 = turnValue(frame, 0);
+                return String.format("%s%s%s%s", SEPARATOR, turn1, SEPARATOR, Constants.MSG_SPARE_PINFALL);
+            }
+            else {
+                String turn1 = turnValue(frame, 0);
+                String turn2 = turnValue(frame, 1);
+                return String.format("%s%s%s%s", SEPARATOR,turn1, SEPARATOR, turn2);
+            }
+        }
+        return "";
+    }
+
+    private static String frameScore(IBowlingGame game, int frameIndex) {
+        return String.valueOf(game.frameScore(frameIndex));
+    }
+    private static String turnValue(BowlingFrame frame, int turnIndex) {
+        if(frame.getTurns() != null && frame.getTurns().size() > turnIndex) {
+            BowlingTurn turn = frame.getTurns().get(turnIndex);
+            return turn.isFoul() ? Constants.MSG_FOUL_PINFALL : String.valueOf(turn.getPins());
+        }
+        return "";
     }
 }
