e0ac31a4a8706407d6c3a2c8e8257e17aa449685
jublar.garcia <jublar@gmail.com>
2019-08-04 14:25:47 -0500

    Method refactor

--- src/main/java/ec/com/java/challenge/bowling/input/FileGameInputReader.java
+++ src/main/java/ec/com/java/challenge/bowling/input/FileGameInputReader.java
@@ -2,11 +2,10 @@ package ec.com.java.challenge.bowling.input;
 
 import ec.com.java.challenge.bowling.input.validator.IGameInputValidator;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -16,28 +15,22 @@ public class FileGameInputReader implements IGameInputReader {
     private String fileName;
     private IGameInputValidator inputValidator;
 
-    public FileGameInputReader(String fileName, IGameInputValidator inputValidator){
+    public FileGameInputReader(String fileName, IGameInputValidator inputValidator) {
         this.fileName = fileName;
         this.inputValidator = inputValidator;
     }
 
     @Override
     public List<String> read() {
-        List<String> fileLines;
-        List<String> validLines = new ArrayList<>();
-        if(fileName != null) {
-            File file = new File(fileName);
-            if(file.exists()) {
-                try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
-                    fileLines = stream.collect(Collectors.toList());
-                    fileLines.stream().filter(inputValidator::validate).forEach(validLines::add);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                System.err.println(String.format("File %s not found.", fileName));
-            }
+        if (fileName == null || !Files.exists(Paths.get(fileName))) {
+            System.err.println(String.format("File %s not found.", fileName));
+            return Collections.emptyList();
         }
-        return validLines;
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+            return stream.filter(inputValidator::validate).collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
     }
 }
