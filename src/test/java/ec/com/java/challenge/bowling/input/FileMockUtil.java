31f6fd6ac19af9b457e914c6a854a8a0bd8f0d56
jublar.garcia <jublar@gmail.com>
2019-08-03 20:36:58 -0500

    New util methods to mock file operations and content

--- src/test/java/ec/com/java/challenge/bowling/input/FileMockUtil.java
+++ src/test/java/ec/com/java/challenge/bowling/input/FileMockUtil.java
@@ -1,11 +1,16 @@
 package ec.com.java.challenge.bowling.input;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class FileMockUtil {
 
-    public final static String FILE_NAME = "input.txt";
+    private final static String FILE_NAME = "input.txt";
     public final static String FILE_PATH = String.format("%s%s%s", System.getProperty("user.dir"), File.separator, FILE_NAME);
 
     public static void createMockFile() {
@@ -21,5 +26,37 @@ public class FileMockUtil {
     public static void deleteMockFile() {
         File file = new File(FileMockUtil.FILE_PATH);
         file.delete();
+    }
+
+    public static void writeToFile(List<String> lines) {
+        BufferedWriter outputWriter = null;
+        try {
+            outputWriter = new BufferedWriter(new FileWriter(FILE_PATH));
+            for (int i = 0; i < lines.size(); i++) {
+                outputWriter.write(lines.get(i));
+                outputWriter.newLine();
+            }
+            outputWriter.flush();
+            outputWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeLinesInFile(int valid, int notValid) {
+        FileMockUtil.createMockFile();
+        List<String> lines = new ArrayList<>();
+        for (int i = 0; i < (valid + notValid); i++) {
+            lines.add(i < valid ? generateValidLine() : generateNotValidLine());
+        }
+        FileMockUtil.writeToFile(lines);
+    }
+
+    private static String generateValidLine() {
+        return String.format("player  %s", new Random().nextInt(11));
+    }
+
+    private static String generateNotValidLine() {
+        return "this is an invalid line in file";
     }
 }
