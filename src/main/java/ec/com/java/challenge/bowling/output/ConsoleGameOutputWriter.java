7a3261c956560c0ec6fb0aa2f3a5d4f908680e61
jublar.garcia <jublar@gmail.com>
2019-08-03 21:51:46 -0500

    New method to write the output from parsed games

--- src/main/java/ec/com/java/challenge/bowling/output/ConsoleGameOutputWriter.java
+++ src/main/java/ec/com/java/challenge/bowling/output/ConsoleGameOutputWriter.java
@@ -1,5 +1,13 @@
 package ec.com.java.challenge.bowling.output;
 
+import ec.com.java.challenge.bowling.game.IBowlingGame;
+
+import java.util.List;
+
 public class ConsoleGameOutputWriter implements IGameOutputWriter {
 
+    @Override
+    public void write(List<IBowlingGame> games) {
+
+    }
 }
--- src/main/java/ec/com/java/challenge/bowling/output/IGameOutputWriter.java
+++ src/main/java/ec/com/java/challenge/bowling/output/IGameOutputWriter.java
@@ -1,4 +1,9 @@
 package ec.com.java.challenge.bowling.output;
 
+import ec.com.java.challenge.bowling.game.IBowlingGame;
+
+import java.util.List;
+
 public interface IGameOutputWriter {
+    void write(List<IBowlingGame> games);
 }
